b0a835052e5ea50ffe8ab0cfc122cc48bf38567b
Joshua Leath <lawnfurniture@icloud.com>
2020-12-14 14:38:39 -0500

    possible broke shuttle while trying to get second part

--- shuttle/shuttle.rb
+++ shuttle/shuttle.rb
@@ -8,11 +8,34 @@ def matches_offset(timestamp, bus_id, offset)
   calculate_departure(bus_id, timestamp) - timestamp == offset
 end
 
+def gcd(x, y)
+  remainder = 0
+  loop do
+    break if y.zero?
+
+    remainder = x % y
+    x = y
+    y = remainder
+  end
+  x
+end
+
+def least_common_multiple(x, y)
+  (x * y) / gcd(x, y)
+end
+
+input_file = File.open('small_input.txt', 'r')
+data = input_file.readlines.map(&:chomp)
+input_file.close
+earliest_departure = data[0].to_i
+buses = data[1].split(',')
+buses = buses.reject {|id| id == 'x' }.map(&:to_i)
+
 def extended_gcd(a, b)
-  old_r, r =  a, b
+  old_r, r = a, b
   old_s, s = 1, 0
   old_t, t = 0, 1
-  while r > 0
+  while r != 0
     quotient, remainder = old_r.divmod(r)
     old_r, r = r, remainder
     old_s, s = s, old_s - quotient * s
@@ -20,13 +43,6 @@ def extended_gcd(a, b)
   end
   [old_r, old_s, old_t]
 end
-
-input_file = File.open('input.txt', 'r')
-data = input_file.readlines.map(&:chomp)
-input_file.close
-earliest_departure = data[0].to_i
-buses = data[1].split(',')
-buses = buses.reject {|id| id == 'x' }.map(&:to_i)
 
 departures = {}
 best_option = nil
@@ -55,15 +71,16 @@ shortest_route = departure_offsets.keys.min
 smallest_offset = departure_offsets.values.min
 
 gcd, s, t = extended_gcd(longest_route, shortest_route)
-advantage = (departure_offsets[longest_route] - departure_offsets[shortest_route]).abs
-increment = longest_route / gcd * shortest_route
-p departure_offsets
-t = 0
-# loop do
-#   if departure_offsets.all? { |id, offset| matches_offset(t, id, offset)}
-#     break
-#   end
-#   t += longest_route
-# end
+
+start_time = Time.now
+t = longest_route - departure_offsets[longest_route]
+loop do
+  if departure_offsets.all? { |id, offset| matches_offset(t, id, offset)}
+    break
+  end
+  t += longest_route
+end
+end_time = Time.now
 
 puts t
+puts "Completed in #{end_time - start_time}"
