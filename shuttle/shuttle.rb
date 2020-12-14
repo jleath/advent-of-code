9db4cec3c90dac8cbac692761372cdeef004a599
Joshua Leath <lawnfurniture@icloud.com>
2020-12-13 21:50:08 -0500

    started shuttle

---
+++ shuttle/shuttle.rb
@@ -0,0 +1,69 @@
+def calculate_departure(id, earliest_timestamp)
+  div, mod = earliest_timestamp.divmod(id)
+  return earliest_timestamp if mod == 0
+  (div * id) + id
+end
+
+def matches_offset(timestamp, bus_id, offset)
+  calculate_departure(bus_id, timestamp) - timestamp == offset
+end
+
+def extended_gcd(a, b)
+  old_r, r =  a, b
+  old_s, s = 1, 0
+  old_t, t = 0, 1
+  while r > 0
+    quotient, remainder = old_r.divmod(r)
+    old_r, r = r, remainder
+    old_s, s = s, old_s - quotient * s
+    old_t, t = t, old_t - quotient * t
+  end
+  [old_r, old_s, old_t]
+end
+
+input_file = File.open('input.txt', 'r')
+data = input_file.readlines.map(&:chomp)
+input_file.close
+earliest_departure = data[0].to_i
+buses = data[1].split(',')
+buses = buses.reject {|id| id == 'x' }.map(&:to_i)
+
+departures = {}
+best_option = nil
+buses.each do |bus_id|
+  earliest = calculate_departure(bus_id, earliest_departure)
+  departures[bus_id] = earliest
+  best_option = bus_id if best_option.nil? || earliest < departures[best_option]
+end
+
+puts best_option * (departures[best_option] - earliest_departure)
+
+# Assume that we have a fixed timestamp t the begins a pattern where each bus
+# departs in manner so that departure - t equals the index of that bus in our input
+# find the earliest timestamp for which this pattern occurs
+
+# start with the naive case
+
+departure_offsets = {}
+data[1].split(',').each_with_index do |bus_id, index| 
+  departure_offsets[bus_id.to_i] = index unless bus_id == 'x'
+end
+
+longest_route = departure_offsets.keys.max
+largest_offset = departure_offsets.values.max
+shortest_route = departure_offsets.keys.min
+smallest_offset = departure_offsets.values.min
+
+gcd, s, t = extended_gcd(longest_route, shortest_route)
+advantage = (departure_offsets[longest_route] - departure_offsets[shortest_route]).abs
+increment = longest_route / gcd * shortest_route
+p departure_offsets
+t = 0
+# loop do
+#   if departure_offsets.all? { |id, offset| matches_offset(t, id, offset)}
+#     break
+#   end
+#   t += longest_route
+# end
+
+puts t
